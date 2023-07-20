6e5495ac95050a445171a219711601a6b826b44c
Chichao Cheung <zhangzhichao@storswift.com>
2023-07-20 15:02:10 +0800

    feat: Add Noir language key words

---
+++ transpiler/sub_module/key_words.py
@@ -0,0 +1,30 @@
+USE = 'use'
+MOD = 'mod'
+LET = 'let'
+MUT = 'mut'
+ASSERT = 'assert'
+THEN = 'then'
+RETURN = 'return'
+IN = 'in'
+PUB = 'pub'
+PRINTLN = 'println'
+DEP_STD_PRINTLN = f'dep::std::{PRINTLN}'
+
+# comptime variables
+COMPTIME = "comptime"
+
+GLOBAL = 'global'
+SELF = 'self'
+
+"""
+"\t" number +1 key words
+"""
+FOR = 'for'
+IF = 'if'
+ELSE = 'else'
+ELIF = f'{ELSE} {IF}'
+FN = 'fn'
+STRUCT = 'struct'
+INLINE = 'inline'
+
+# GET_TABLE_P_ONE = [FOR, IF, ELSE, ELIF, FN, STRUCT]
