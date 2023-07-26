bc4063217b32ca02ca430e02ae9ecba08895d9aa
Chichao Cheung <zhangzhichao@storswift.com>
2023-07-26 15:51:09 +0800

    build: add setup.py to help external module usage

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='python2noir',
+    version='0.1',
+    description='Easy to quickly convert python libraries to Noir contracts',
+    author='storswift',
+    author_email='',
+    packages=['transpiler'],
+)
